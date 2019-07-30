0fdeeaf869f9df8c444ce6466c49eaf25f628e58
Aude Faucheux <aude53@gmail.com>
2019-07-30 20:53:41 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,7 +55,7 @@ end
 def checkout(cart, coupons)
   consol_cart = consolidate_cart(cart)
   cart_with_coupons = apply_coupons(consol_cart, coupons)
-  cart_with_discounts = apply_clearance(cart_with_coupons_applied)
+  cart_with_discounts = apply_clearance(cart_with_coupons)
 
   total = 0.0
   cart_with_discounts.keys.each do |item|
